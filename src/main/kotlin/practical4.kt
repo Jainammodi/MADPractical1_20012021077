0bc0bde709bd6c3678e59362a9db8e615f3e428b
Jainammodi <jainambmodi1473@gmail.com>
2022-08-09 08:51:28 +0530

    pr-4

---
+++ src/main/kotlin/practical4.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    println("Enter the number")
+    var num= readLine()!!.toInt()
+        if (num%2==0) {
+            print("$num is even")
+        } else {
+            ("$num is odd")
+        }
+
+}
