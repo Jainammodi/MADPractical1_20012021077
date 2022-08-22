c2855651ce04a4e619f3b24b32e06d4c00103001
JAINAM147 <jainammodi20@gnu.ac.in>
2022-08-22 21:14:45 +0530

    practical

--- src/main/kotlin/practical4.kt
+++ src/main/kotlin/practical4.kt
@@ -1,11 +1,9 @@
-fun main()
-{
+fun main() {
     println("Enter the number")
-    var num= readLine()!!.toInt()
-        if (num%2==0) {
-            print("$num is even")
-        } else {
-            ("$num is odd")
-        }
-
+    var num = readLine()!!.toInt()
+    if (num % 2 == 0) {
+        print("$num is even")
+    } else {
+        print("$num is odd")
+    }
 }
--- src/main/kotlin/practical5.kt
+++ src/main/kotlin/practical5.kt
@@ -1,10 +1,20 @@
- fun main()
- {
-     fun opration(x:Char):Int=
-         when (x) {
-             '1'-> {print()
+fun main(){
+    print("Enter Month Number: ")
+    var m:Int = readln()!!.toInt()
 
-             else-> print("error")
-         }
-
+    when(m){
+        1 -> print("January")
+        2 -> print("February")
+        3 -> print("March")
+        4 -> print("April")
+        5 -> print("May")
+        6 -> print("June")
+        7 -> print("July")
+        8 -> print("August")
+        9 -> print("September")
+        10 -> print("October")
+        11 -> print("Nuvember")
+        12 -> print("December")
+        else -> print("Enter proper number")
+    }
 }
